573a962097505651ef501f9a2a9debc2a5e21648
Ifeoluwa <adelekanifeoluwa1402@gmail.com>
2025-06-09 10:16:33 -0700

    upload avatar

--- server/api/user/upload-avatar.post.js
+++ server/api/user/upload-avatar.post.js
@@ -1,6 +1,7 @@
 import formidable from 'formidable'
-import path from 'path'
+import path, { join } from 'path'
 import { useMongooseModel } from '~/server/utils/useMongooseModel'
+
 export const config = {
   api: {
     bodyParser: false
@@ -8,13 +9,16 @@ export const config = {
 }
 
 export default defineEventHandler(async (event) => {
-  const form = formidable({ uploadDir: 'public/uploads', keepExtensions: true})
+  const uploadDir = join(process.cwd(), 'public', 'uploads')
+  const form = formidable({ uploadDir, keepExtensions: true })
+
   const [fields, files] = await new Promise((resolve, reject) => {
     form.parse(event.req, (err, fields, files) => {
       if (err) reject(err)
       resolve([fields, files])
     })
   })
+
   const userId = fields.userId
   const file = files.avatar[0]
   const filename = path.basename(file.filepath)
@@ -22,10 +26,14 @@ export default defineEventHandler(async (event) => {
 
   const User = useMongooseModel('User')
   const user = await User.findById(userId)
-  if(!user) {
-    throw createError({ statusCode: 404, message: 'User not found!'})
+  if (!user) {
+    throw createError({ statusCode: 404, message: 'User not found!' })
   }
+
   user.avatar = avatarPath
   await user.save()
-  return { avatarUrl: avatarPath}
+
+  console.log('Avatar saved at:', avatarPath)
+
+  return { avatarUrl: avatarPath }
 })
